8893dca058aab5cc12398448e1bbf75a2a3b97fc
Marcus Kincaid <marcus@queldorei.com>
2017-11-09 19:04:32 +0200

    Navigation block upgrade. Some attrs realized on frontend

--- app/code/WDPH/Megamenu/Block/Html/Navigation.php
+++ app/code/WDPH/Megamenu/Block/Html/Navigation.php
@@ -3,9 +3,11 @@ namespace WDPH\Megamenu\Block\Html;
 
 class Navigation extends \Magento\Catalog\Block\Navigation
 {
-	protected $megamenuHelper;
+	public $megamenuHelper;
 	protected $catalogCategoryHelper;
 	protected $flatState;
+	protected $storeManager;
+	protected $filterProvider;
 	
     public function __construct(\Magento\Framework\View\Element\Template\Context $context,
 								\Magento\Catalog\Model\CategoryFactory $categoryFactory,
@@ -15,13 +17,17 @@ class Navigation extends \Magento\Catalog\Block\Navigation
 								\Magento\Catalog\Helper\Category $catalogCategoryHelper,
 								\Magento\Framework\Registry $registry,
 								\Magento\Catalog\Model\Indexer\Category\Flat\State $flatState,
+								\Magento\Store\Model\StoreManagerInterface $storeManager,
+								\Magento\Cms\Model\Template\FilterProvider $filterProvider,
 								\WDPH\Megamenu\Helper\Data $megamenuHelper,
 								array $data = [])
 	{
 		$this->flatState = $flatState;
 		$this->catalogCategoryHelper = $catalogCategoryHelper;
+		$this->storeManager = $storeManager;
+		$this->filterProvider = $filterProvider;
 		$this->megamenuHelper = $megamenuHelper;
-		parent::__construct($context, $categoryFactory, $productCollectionFactory, $layerResolver, $httpContext, $catalogCategoryHelper, $registry, $flatState, $data);
+		parent::__construct($context, $categoryFactory, $productCollectionFactory, $layerResolver, $httpContext, $catalogCategoryHelper, $registry, $flatState, $data);		
     }
 	
 	public function renderCategoriesMenuHtml()
@@ -35,6 +41,7 @@ class Navigation extends \Magento\Catalog\Block\Navigation
             }
         }
 		$html = '';
+		$html .= $this->renderHomeItem();
 		foreach ($activeCategories as $category)
 		{			
             $html .= $this->renderCategoryMenuItemHtml($category);
@@ -42,21 +49,30 @@ class Navigation extends \Magento\Catalog\Block\Navigation
 		return $html;
 	}
 	
+	protected function getStoreCategories()
+    {
+        return $this->catalogCategoryHelper->getStoreCategories();
+    }
+	
 	protected function renderCategoryMenuItemHtml($category, $level = 0)
 	{		
-		if (!$category->getIsActive())
+		if (!$category->getIsActive() || $category->getData('wdph_megamenu_hide_item'))
 		{
             return '';
         }
 		$html = '';
-		$prefix = '';
-		$levelLocal = $level;
-		while($levelLocal > 0)
+		$additionalLiClasses = '';
+		$catDropDownType = $category->getData('wdph_megamenu_dropdown_type');
+		$catAddLabel = $category->getData('wdph_megamenu_cat_label');
+		if($catAddLabel)
 		{
-			$prefix .= '-';
-			$levelLocal--;
-		}	
-		$html .= '<p>' . $prefix . $this->escapeHtml($category->getName()) . '</p>';
+			$catAddLabel = '<span class="wdph-megamenu-item-add-label">' . $this->megamenuHelper->getConfig('general/' . $catAddLabel) . '</span>';
+		}			
+		$additionalLiClasses .= ' wdph-dropdown-type-' . ($catDropDownType ? $catDropDownType : $this->megamenuHelper->getConfig('general/menu_type'));
+		$additionalLiClasses .= ($category->getData('wdph_megamenu_float') ? ' wdph-item-float-' . $category->getData('wdph_megamenu_float') : '');
+		$html .= '<li class="wdph-megamenu-item level-' . $level . ' ' . $additionalLiClasses . '"><a class="" href="' .
+					(trim($category->getData('wdph_megamenu_item_url')) ? trim($category->getData('wdph_megamenu_item_url')) : $this->getCategoryUrl($category)) .
+					'"><span class="wdph-megamenu-item-label">' . $this->escapeHtml($category->getName()) . '</span>' . $catAddLabel . '</a>';
 		if ($this->flatState->isFlatEnabled())
 		{			
             $children = (array)$category->getChildrenNodes();
@@ -68,18 +84,80 @@ class Navigation extends \Magento\Catalog\Block\Navigation
 			$childrenCount = $children->count();			
         }
 		if($childrenCount)
-		{			
+		{
+			$dropdownTopBlock = '';
+			$dropdownBottomBlock = '';
+			$dropdownLeftBlock = '';
+			$dropdownRightBlock = '';
+			if($catDropDownType != 'classic' && $level == 0)
+			{				
+				$dropdownTopBlock = $this->getCategoryDropdownAdditionalContent($category, 'wdph_megamenu_top_block_con');
+				if($dropdownTopBlock)
+				{
+					$dropdownTopBlock = '<div class="wdph_megamenu-dropdown-top">' . $dropdownTopBlock . '</div>';
+				}
+				$dropdownBottomBlock = $this->getCategoryDropdownAdditionalContent($category, 'wdph_megamenu_bot_block_cont');
+				if($dropdownBottomBlock)
+				{
+					$dropdownBottomBlock = '<div class="wdph_megamenu-dropdown-bottom">' . $dropdownBottomBlock . '</div>';
+				}
+				$dropdownLeftBlock = $this->getCategoryDropdownAdditionalContent($category, 'wdph_megamenu_left_block_cont');
+				if($dropdownLeftBlock)
+				{
+					$dropdownLeftBlock = '<div class="wdph_megamenu-dropdown-left"' . (trim($category->getData('wdph_megamenu_left_block_w')) ? ' style="width:"' . $category->getData('wdph_megamenu_left_block_w') . ';"' : '') . '>' . $dropdownLeftBlock . '</div>';
+				}
+				$dropdownRightBlock = $this->getCategoryDropdownAdditionalContent($category, 'wdph_megamenu_right_block_cont');
+				if($dropdownRightBlock)
+				{
+					$dropdownRightBlock = '<div class="wdph_megamenu-dropdown-right"' . (trim($category->getData('wdph_megamenu_left_block_w')) ? ' style="width:"' . $category->getData('wdph_megamenu_right_block_w') . ';"' : '') . '>' . $dropdownRightBlock . '</div>';
+				}
+			}
+			$html .= '<div class="wdph-megamenu-submenu level' . $level . '">' . $dropdownTopBlock . $dropdownLeftBlock . '<ul class="">';
 			foreach ($children as $child)
 			{
 				$html .= $this->renderCategoryMenuItemHtml($child, $level + 1);
 			}
+			$html .= '</ul>' . $dropdownBottomBlock . $dropdownRightBlock . '</div>';
 		}
+		$html .= '</li>';
 		return $html;
 	}
 	
-	public function getStoreCategories()
-    {
-        return $this->catalogCategoryHelper->getStoreCategories();
-    }
+	protected function renderHomeItem()
+	{
+		if($this->megamenuHelper->getConfig('general/home'))
+		{
+			return '<li class="wdph-megamenu-item level-0"><a class="" href="' . $this->storeManager->getStore()->getBaseUrl() . '"><span class="wdph-megamenu-item-label">' . __('Home') . '</span></a></li>';
+		}
+		return '';
+	}
+	
+	protected function getCategoryDropdownAdditionalContent($category, $attrId)
+	{
+		return $this->filterProvider->getBlockFilter()->filter(trim($category->getData($attrId)));
+	}
+	
+	public function renderBlockBeforeMenu()
+	{
+		if($this->megamenuHelper->getConfig('general/staticblock_before'))
+		{
+			return $this->getCMSBlock($this->megamenuHelper->getConfig('general/staticblock_before'));
+		}
+	}
+	
+	public function renderBlockAfterMenu()
+	{
+		if($this->megamenuHelper->getConfig('general/staticblock_after'))
+		{
+			return $this->getCMSBlock($this->megamenuHelper->getConfig('general/staticblock_after'));
+		}
+	}
+	
+	protected function getCMSBlock($blockId)
+	{
+		return $this->getLayout()->createBlock('Magento\Cms\Block\Block')->setBlockId($blockId)->toHtml();
+		//Ptovider for getting html content from attribute 
+		//return $this->filterProvider->getBlockFilter()->filter(trim($blockId));
+	}	
 }
 ?>
