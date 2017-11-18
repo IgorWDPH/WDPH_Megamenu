c3076ff9643f8eaa543af826825971de00f58579
Marcus Kincaid <marcus@queldorei.com>
2017-11-18 02:31:19 +0200

    Item Background Color attr added

--- app/code/WDPH/Megamenu/Setup/InstallData.php
+++ app/code/WDPH/Megamenu/Setup/InstallData.php
@@ -143,6 +143,15 @@ class InstallData implements InstallDataInterface
             'group'    	=> 'WDPH Megamenu',
         ]);
 		
+		$eavSetup->addAttribute(\Magento\Catalog\Model\Category::ENTITY, 'wdph_megamenu_item_back_c', [
+            'type' 		=> 'varchar',
+            'label'    	=> 'Item Background Color',
+            'input' 	=> 'text',
+            'required' 	=> false,
+            'global'   	=> \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_STORE,
+            'group'    	=> 'WDPH Megamenu',
+        ]);
+		
 		$eavSetup->addAttribute(\Magento\Catalog\Model\Category::ENTITY, 'wdph_megamenu_item_hover_c', [
             'type' 		=> 'varchar',
             'label'    	=> 'Item Hover Background Color',
